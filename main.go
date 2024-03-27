5da0ae286995cdf9704e8f72f133e0d188a4c144
winmord <winmord@yandex.ru>
2024-03-27 23:47:03 +0300

    feature: add initial code

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "net/http"
+
+func main() {
+	webDir := "./web"
+	http.Handle("/", http.FileServer(http.Dir(webDir)))
+
+	err := http.ListenAndServe(":7540", nil)
+	if err != nil {
+		panic(err)
+	}
+}
