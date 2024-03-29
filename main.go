a71a12df74dd9aba088c0ca7f55d63d47bee990f
winmord <winmord@yandex.ru>
2024-03-29 21:58:46 +0300

    feature: make first part task

--- main.go
+++ main.go
@@ -1,12 +1,29 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+func getPort() int {
+	port := 7540
+	envPort := os.Getenv("TODO_PORT")
+	if len(envPort) > 0 {
+		if eport, err := strconv.ParseInt(envPort, 10, 32); err == nil {
+			port = int(eport)
+		}
+	}
+
+	return port
+}
 
 func main() {
-	webDir := "./web"
+	webDir := "web"
 	http.Handle("/", http.FileServer(http.Dir(webDir)))
 
-	err := http.ListenAndServe(":7540", nil)
+	err := http.ListenAndServe(fmt.Sprintf(":%d", getPort()), nil)
 	if err != nil {
 		panic(err)
 	}
