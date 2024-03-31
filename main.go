45e324c96f354fbbf75080a902d922fd776922f2
winmord <winmord@yandex.ru>
2024-03-31 14:52:06 +0300

    feature: start implementing of fourth task

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bytes"
 	"database/sql"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"log"
@@ -22,6 +24,8 @@ type Sheduler struct {
 	Comment string
 	Repeat  string
 }
+
+var db *sql.DB
 
 func getDbFilePath() string {
 	//appPath, err := os.Executable()
@@ -45,7 +49,7 @@ func createDbFile(dbFilePath string) (*sql.DB, error) {
 		return nil, err
 	}
 
-	db, err := sql.Open("sqlite3", dbFilePath)
+	db, err = sql.Open("sqlite3", dbFilePath)
 	if err != nil {
 		return nil, err
 	}
@@ -54,7 +58,8 @@ func createDbFile(dbFilePath string) (*sql.DB, error) {
 }
 
 func createTable(db *sql.DB) {
-	_, err := db.Exec("CREATE TABLE `scheduler` (`id` INTEGER PRIMARY KEY AUTOINCREMENT, `date` VARCHAR(8) NOT NULL, `title` VARCHAR(64) NOT NULL, `comment` VARCHAR(255) NOT NULL, `repeat` VARCHAR(128) NULL)")
+	_, err := db.Exec(
+		"CREATE TABLE `scheduler` (`id` INTEGER PRIMARY KEY AUTOINCREMENT, `date` VARCHAR(8) NULL, `title` VARCHAR(64) NOT NULL, `comment` VARCHAR(255) NULL, `repeat` VARCHAR(128) NULL)")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -159,6 +164,100 @@ func getNextDate(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+type Task struct {
+	Date    string
+	Title   string
+	Comment string
+	Repeat  string
+}
+
+func insertTask(db *sql.DB, task Task) (int, error) {
+	res, err := db.Exec("INSERT INTO scheduler (date, title, comment, repeat) VALUES (:date, :title, :comment, :repeat)",
+		sql.Named("date", task.Date),
+		sql.Named("title", task.Title),
+		sql.Named("comment", task.Comment),
+		sql.Named("repeat", task.Repeat))
+
+	if err != nil {
+		return 0, err
+	}
+	id, err := res.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	return int(id), nil
+}
+
+type TaskIdResponse struct {
+	Id int `json:"id"`
+}
+
+type ErrorResponse struct {
+	Error string `json:"error"`
+}
+
+func addTask(w http.ResponseWriter, r *http.Request) {
+	var taskData Task
+	var buffer bytes.Buffer
+
+	if _, err := buffer.ReadFrom(r.Body); err != nil {
+		http.Error(w, fmt.Errorf("body getting error: %w", err).Error(), http.StatusBadRequest)
+		return
+	}
+
+	if err := json.Unmarshal(buffer.Bytes(), &taskData); err != nil {
+		http.Error(w, fmt.Errorf("JSON encoding error: %w", err).Error(), http.StatusBadRequest)
+		return
+	}
+
+	// TODO: add checks
+	date, err := time.Parse(datePattern, taskData.Date)
+	if err != nil {
+		http.Error(w, fmt.Errorf("bad date format: %w", err).Error(), http.StatusBadRequest)
+		return
+	}
+
+	if len(taskData.Date) == 0 {
+		taskData.Date = time.Now().Format(datePattern)
+	}
+
+	if date.Before(time.Now()) {
+		if len(taskData.Repeat) > 0 {
+			taskData.Date, _ = NextDate(time.Now(), taskData.Date, taskData.Repeat)
+		} else {
+			taskData.Date = time.Now().Format(datePattern)
+		}
+	}
+
+	if len(taskData.Title) == 0 {
+		errorResponse, err := json.Marshal(ErrorResponse{"title can not be empty"})
+		if err != nil {
+			http.Error(w, fmt.Errorf("failed to create task: %w", err).Error(), http.StatusBadRequest)
+			return
+		}
+
+		w.Write(errorResponse)
+		return
+	}
+
+	taskId, err := insertTask(db, taskData)
+	if err != nil {
+		http.Error(w, fmt.Errorf("failed to create task: %w", err).Error(), http.StatusBadRequest)
+		return
+	}
+
+	taskIdData, err := json.Marshal(TaskIdResponse{taskId})
+
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusCreated)
+	_, err = w.Write(taskIdData)
+
+	if err != nil {
+		http.Error(w, fmt.Errorf("writing task id error: %w", err).Error(), http.StatusBadRequest)
+	}
+}
+
 func main() {
 	installDb()
 	webDir := "./web"
@@ -166,6 +265,7 @@ func main() {
 	r := chi.NewRouter()
 	r.Mount("/", http.FileServer(http.Dir(webDir)))
 	r.Get("/api/nextdate", getNextDate)
+	r.Post("/api/task", addTask)
 
 	err := http.ListenAndServe(fmt.Sprintf(":%d", getPort()), r)
 	if err != nil {
